188a5a5cc5919b67ad10eda25f07ca765848f555
fazrilr21 <fazrilramdani6@gmail.com>
2024-01-16 13:51:40 +0700

    edit

--- script.js
+++ script.js
@@ -1,45 +1,93 @@
-// let angka = document.getElementById('angka');
-// let deleteAll = document.getElementById('deleteAll');
-// let deleteOne   = document.getElementById('deleteOne');
-// let bagi = document.getElementById ('bagi');
-// let kurang = document.getElementById ('kurang');
-// let jumlah = document.getElementById ('jumlah'); 
-// let kali = document.getElementById ('kali');
-// let koma = document.getElementById ('koma');
-// let samaDengan = document.getElementById ('samaDengan');
-// let result = document.getElementById('result');
-// let hasil = document.getElementById('hasil');
+const fungsiAll = document.getElementById('fungsiAll') ;
+const fungsiOne = document.querySelector('#fungsiOne') ;
+const number = document.querySelectorAll ('.number');
+const arithmetic = document.querySelectorAll ('#arithmetic');
+const hasil = document.querySelector('.hasil')
+const equals = document.querySelector('#equals')
+
+let firstValue = "";
+let isFirstValue = false;
+let secondValue = "";
+let isSecondValue = false
+let arit = "";
+let isArit = 0;
 
 
-// angka.addEventListener("click", function(){
-//     hasil.textContent = angka.value
-// });
 
-document.addEventListener("DOMContentLoaded", function () {
-    var display = document.getElementById("hasil");
-    var buttons = document.getElementsByTagName("button");
-
-    for (var i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener("click", function () {
-            handleButtonClick(this.value);
-        });
-    }
-
-    function handleButtonClick(value) {
-        if (value === "=") {
-            try {
-                display.textContent = eval(display.textContent);
-            } catch (error) {
-                display.textContent = "Error";
-            }
-        } else if (value === "C" || value === "AC") {
-            display.textContent = "0";
-        } else {
-            if (display.textContent === "0" || display.textContent === "Error") {
-                display.textContent = value;
-            } else {
-                display.textContent += value;
-            }
+for(let i = 0; i < number.length ; i++ ){
+    number[i].addEventListener('click' , (e)=>{
+        let atr = e.target.getAttribute('value');
+        if(isFirstValue === false){
+            getFirstValue(atr)
+        }if(isSecondValue === false){
+            getSecondValue(atr)
         }
+    })
+}
+
+function getFirstValue(el){
+    hasil.innerHTML = "";
+    firstValue +=el;
+    hasil.innerHTML = firstValue
+    firstValue = +firstValue
+}
+function getSecondValue(el){
+    if(firstValue != "" && arit != ""){
+        secondValue +=el
+        hasil.innerHTML = secondValue
+        secondValue = +secondValue
     }
-});
+}
+
+function getAritmethic(){
+    for (let i = 0 ; i < arithmetic.length ; i++){
+        arithmetic[i].addEventListener('click', (e) => {
+            arit = e.target.getAttribute('value');
+            isFirstValue = true
+            arithmetic.style.background = 'blue'
+        })
+    }
+}
+
+getAritmethic()
+
+
+equals.addEventListener('click' , ()=>{
+    hasil.innerHTML ="";
+    if (arit === "+"){
+        hasilValue = firstValue + secondValue
+    }
+    else if (arit === "-"){
+        hasilValue = firstValue - secondValue
+    }
+    else if (arit === "/"){
+        hasilValue = firstValue / secondValue
+    }
+    else if (arit === "*"){
+        hasilValue = firstValue * secondValue
+    }
+    hasil.innerHTML = hasilValue
+    firstValue =hasilValue
+    secondValue =""
+})
+
+
+function checkResultLength(){
+    hasilValue = JSON.stringify(hasilValue);
+
+    if(hasilValue.length >+ 8){
+        hasilValue = JSON.parse(hasilValue)
+        hasil.innerHTML = hasilValue.toFixed(5)
+    }
+}
+
+fungsiAll.addEventListener('click', ()=>{
+    hasil.innerHTML = 0;
+
+    firstValue = "";
+    isFirstValue = false;
+    secondValue = "";
+    isSecondValue = false
+    arit = "";
+    hasilValue = 0;
+})
